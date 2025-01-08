736f475db8706f8806a65cf4de5ff377351c9b03
Jacob Cooley <jacobcooley2051@gmail.com>
2025-01-08 18:37:03 -0500

    Created project

---
+++ firmware/particle-count-sensor-pwm/main/include/particle-count-sensor-pwm.h
@@ -0,0 +1,7 @@
+#ifndef __PARTICLE_COUNT_SENSOR_PWM__H__
+#define __PARTICLE_COUNT_SENSOR_PWM__H__
+
+void app_main(void);
+
+
+#endif
---
+++ firmware/particle-count-sensor-pwm/main/particle-count-sensor-pwm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define PWM_INPUT_GPIO 18
+
+
+void app_main(void)
+{
+
+}
